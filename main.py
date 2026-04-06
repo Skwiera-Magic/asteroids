e927b5d40e3e6c88215bc27a52902cc31143d6b5
skwiera_magic <mckskwiera@gmail.com>
2026-04-06 17:59:06 +0100

    player rotating works

--- main.py
+++ main.py
@@ -2,26 +2,26 @@ import pygame
 from constants import *
 from logger import log_state
 from circleshape import *
-from player import Player
+from player import *
 
 def main():
     pygame.init()
     print("Starting Asteroids with pygame version:", pygame.version.ver)
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
-    my_player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     timer = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    my_player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        my_player.update(dt)
         my_player.draw(screen)
         pygame.display.flip()
-        timer.tick(60)
         dt = timer.tick(60) / 1000
 
 if __name__ == "__main__":
