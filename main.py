139dc24b79ffd0e1c2236f70db96a7e12dfc2649
skwiera_magic <mckskwiera@gmail.com>
2026-04-06 22:38:48 +0100

    shooting works

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pygame
 import sys
 from constants import *
-from logger import *
-from circleshape import *
-from player import *
-from asteroid import *
-from asteroidfield import *
+from logger import log_state, log_event
+from circleshape import CircleShape
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -15,12 +16,14 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     timer = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable,)
+    Shot.containers = (shots, updatable, drawable)
     AsteroidField()
     my_player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
 from circleshape import *
 from constants import *
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -32,9 +33,18 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def move(self, dt):
         unit_vector = pygame.Vector2(0, 1)
         rotated_vector = unit_vector.rotate(self.rotation)
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
         self.position += rotated_with_speed_vector
+
+    def shoot(self):
+        bullet = Shot(self.position.x, self.position.y)
+        bullet_vector = pygame.Vector2(0, 1)
+        rotated_bullet = bullet_vector.rotate(self.rotation)
+        shot_bullet = rotated_bullet * PLAYER_SHOOT_SPEED
+        bullet.velocity = shot_bullet
