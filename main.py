c3178ce7489f1806dfe34da16942d411219d625e
skwiera_magic <mckskwiera@gmail.com>
2026-04-06 17:38:34 +0100

    player renders

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import pygame
 from constants import *
 from logger import log_state
 from circleshape import *
+from player import Player
 
 def main():
     pygame.init()
     print("Starting Asteroids with pygame version:", pygame.version.ver)
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
+    my_player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     timer = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -17,10 +19,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        my_player.draw(screen)
         pygame.display.flip()
         timer.tick(60)
         dt = timer.tick(60) / 1000
-
 
 if __name__ == "__main__":
     main()
